7d2ae16d81bed40ffd4fef93a32533d41182217e
Rodrigo Mariano Ferreira <rmf971359475@gmail.com>
2023-03-07 17:49:55 -0300

    Introducao

---
+++ src/semana03/Introducao.java
@@ -0,0 +1,41 @@
+package semana03;
+
+public class Introducao {
+    public static void main(String[] args) {
+        // Comentario de uma linha
+
+        /*
+         * Comentário de várias 
+         * linhas
+         */
+
+         // Tipos Primitivos
+         // Inteiros
+         // byte, short, int, long
+
+         byte n1 = 100; // 1 byte - -128 a 127
+         short n2 = 3000; // 2 bytes - -32768 a 32767
+         int n3 = 321312321; // 4 bytes - 2 bilhoes
+         long n4 = 3232323232L; // 8 bytes
+
+         // Ponto flutuante
+         float nota1 = 10.3446f;
+         double nota2 = 10.944964;
+
+         // char 
+         char letra = 'A';
+
+         // boolean
+         boolean ligado = true;
+         boolean teste = false;
+
+         // String
+         String nome = "Rodrigo Mariano Ferreira";
+         System.out.println(nome);
+         System.out.println(nome.toLowerCase());
+         System.out.println(nome.toUpperCase());
+
+         letra
+
+    }
+}
